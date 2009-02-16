77fba2243745dc6d0a2c712813b8c52f332e9752
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-02-16 18:00:39 +0100

    Changed version in History. Removed bin folder. Deleted useless examples.

--- examples/wiimote_buttons.rb
+++
@@ -1,7 +0,0 @@
-require 'wiiuse'
-
-WiiUse.init
-WiiUse.poll do |event, wiimote|
-  puts "BUTTON_A" if wiimote.is_pressed?(Wiimote::BUTTON_A)
-  puts "BUTTON_B" if wiimote.is_pressed?(Wiimote::BUTTON_B)
-end
